29dc750d33211cc58126e61fdb4ced80e94f5a8a
Syed Ibrahim <syedibrahimayaan@gmail.com>
2024-07-16 21:54:45 +0530

    first commit

---
+++ Springbootproj/src/main/java/com/syed/Springbootproj/SpringbootprojApplication.java
@@ -0,0 +1,13 @@
+package com.syed.Springbootproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprojApplication.class, args);
+	}
+
+}
---
+++ Springbootproj/src/test/java/com/syed/Springbootproj/SpringbootprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syed.Springbootproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
